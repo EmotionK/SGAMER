5f71d850d4a15c6527b4cd992d4cd7de883f5037
EmotionX <goulx_mail@163.com>
2022-11-12 11:39:32 +0800

    '11'

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 import os
 
 from data_processing import data_processing
-from model.embedding_node import embedding_category_brand
-from model.embedding_user_item import embedding_user_item
-from model.generate_paths import gen_instances
-from model.item_item_representation import item_item_repersentation
-from model.meta_path_instances_representation import meta_path_instances_representation
-from model.recommendation_model import recommendation_model
-from model.user_history import user_history
-from model.user_item_representation import user_item_representation
+#from model.embedding_user_item import embedding_user_item
+#from model.embedding_node import embedding_node
+#from model.generate_paths import gen_instances
+#from model.item_item_representation import item_item_repersentation
+#from model.meta_path_instances_representation import meta_path_instances_representation
+#from model.recommendation_model import recommendation_model
+#from model.user_history import user_history
+#from model.user_item_representation import user_item_representation
 
 base_path = os.getcwd() + '/' #获取当前路径
 choose_dataset = 1 #1:Amazon_Music_Instrument
@@ -23,7 +23,7 @@ user_number,item_number,category_number,brand_number = data_processing(dataset_n
 #embedding_user_item(dataset_name,user_number,item_number)
 
 print(f'{dataset_name} embedding_category_brand.....')
-embedding_node(dataset_name,user_number,item_number)
+#embedding_node(dataset_name,user_number,item_number)
 
 print(f'{dataset_name} item_item_repersentation.....')
 #item_item_repersentation(dataset_name)
