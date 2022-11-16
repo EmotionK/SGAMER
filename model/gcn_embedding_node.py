05ad4af3bd5e1a7ef4450aad691c456cded16395
EmotionK <goulx_mail@163.com>
2022-11-16 10:14:49 +0800

    11

--- model/gcn_embedding_node.py
+++ model/gcn_embedding_node.py
@@ -4,46 +4,18 @@ import torch
 import pandas as pd
 #from networkx import edges
 from torch.nn import Parameter, Module, functional as F
+from torch_geometric.data import Data
+from torch_geometric.nn import GCNConv
 
 dataset_name = 'Amazon_Musical_Instruments'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-class GraphConvolution(Module):
-    def __init__(self, in_features, out_features, bias=True):
-        super(GraphConvolution, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
-        if bias:
-            self.bias = Parameter(torch.FloatTensor(out_features))
-        else:
-            self.register_parameter('bias', None)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.weight.size(1))
-        self.weight.data.uniform_(-stdv, stdv)
-        if self.bias is not None:
-            self.bias.data.uniform_(-stdv, stdv)
-
-    def forward(self, input, adj):
-        support = torch.mm(input, self.weight)
-        output = torch.spmm(adj, support)
-        if self.bias is not None:
-            return output + self.bias
-        else:
-            return output
-
-    def __repr__(self):
-        return self.__class__.__name__ + ' (' \
-               + str(self.in_features) + ' -> ' \
-               + str(self.out_features) + ')'
 
 class GCN(torch.nn.Module):
     def __init__(self, nfeat, nhid, nclass):
         super(GCN, self).__init__()
-        self.gc1 = GraphConvolution(nfeat, nhid)
-        self.gc2 = GraphConvolution(nhid, nclass)
+        self.gc1 = GCNConv(nfeat, nhid)
+        self.gc2 = GCNConv(nhid, nclass)
     def forward(self, x, adj):
         x = F.relu(self.gc1(x, adj))
         x = self.gc2(x, adj)
